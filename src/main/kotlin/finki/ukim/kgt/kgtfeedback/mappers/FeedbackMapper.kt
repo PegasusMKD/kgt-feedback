01e94800d30c4ab93950fe9762a00272dcbf8e8e
PegasusMKD <csprogrammers@outlook.com>
2022-05-18 12:13:28 +0200

    Add null-checks to mapper

--- src/main/kotlin/finki/ukim/kgt/kgtfeedback/mappers/FeedbackMapper.kt
+++ src/main/kotlin/finki/ukim/kgt/kgtfeedback/mappers/FeedbackMapper.kt
@@ -4,8 +4,14 @@ import finki.ukim.kgt.kgtfeedback.dtos.FeedbackDto
 import finki.ukim.kgt.kgtfeedback.models.Feedback
 import org.mapstruct.Mapper
 import org.mapstruct.MappingTarget
+import org.mapstruct.NullValueCheckStrategy
+import org.mapstruct.NullValuePropertyMappingStrategy
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+    nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
 interface FeedbackMapper {
     fun toDto(entity: Feedback?): FeedbackDto?
     fun updateEntity(dto: FeedbackDto?, @MappingTarget entity: Feedback?)
